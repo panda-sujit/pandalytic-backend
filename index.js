279b89b6e8db58bf528aad1c7473517cec477b51
Sujit Libi <sujit_libi@hotmail.com>
2020-08-10 21:39:21 +0545

    Minor Change

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ const express = require('express');
 // const morgan = require('morgan');
 
 const app = express();
-// const corsOption = {
-//   origin: true,
-//   credentials: true,
-//   methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], // to works well with web app, OPTIONS is required
-//   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
+const corsOption = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], // to works well with web app, OPTIONS is required
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
+}
+app.use(cors(corsOption));
+app.options('*', cors(corsOption));
+
+// app.use(function (req, res, next) {
+// res.header("Access-Control-Allow-Origin", "*");
+// res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
+// next();
+
+// res.header("Access-Control-Allow-Origin", "*") //* to give access to any origin
+// res.header(
+//   "Access-Control-Allow-Headers",
+//   "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token" //to give access to all the headers provided
+// );
+// if (req.method === 'OPTIONS') {
+//   res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
+//   return res.status(200).json({});
 // }
-// app.use(cors(corsOption));
-// app.options('*', cors(corsOption));
-
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
-  // next();
-
-  res.header("Access-Control-Allow-Origin", "*") //* to give access to any origin
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token" //to give access to all the headers provided
-  // );
-  // if (req.method === 'OPTIONS') {
-  //   res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
-  //   return res.status(200).json({});
-  // }
-  next();
-});
+// next();
+// });
 
 require('./startup/logging')();
 require('./startup/routes')(app);
