7ac55317f16c0fdb190d97915d62dd8bc45a25bf
Sujit Libi <sujit_libi@hotmail.com>
2020-08-10 22:08:23 +0545

    Minor Change

--- index.js
+++ index.js
@@ -4,32 +4,15 @@ const express = require('express');
 // const morgan = require('morgan');
 
 const app = express();
-const corsOption = {
-  'allowedHeaders': ['Authorization', 'Content-Type'],
-  'origin': '*',
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
-}
-app.use(cors(corsOption));
-app.options('*', cors(corsOption));
-
-// app.use(function (req, res, next) {
-// res.header("Access-Control-Allow-Origin", "*");
-// res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
-// next();
-
-// res.header("Access-Control-Allow-Origin", "*") //* to give access to any origin
-// res.header(
-//   "Access-Control-Allow-Headers",
-//   "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token" //to give access to all the headers provided
-// );
-// if (req.method === 'OPTIONS') {
-//   res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
-//   return res.status(200).json({});
+// const corsOption = {
+//   'allowedHeaders': ['Authorization', 'Content-Type'],
+//   'origin': '*',
+//   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   'preflightContinue': false
 // }
-// next();
-// });
+// app.use(cors(corsOption));
+// app.options('*', cors(corsOption));
+
 
 require('./startup/logging')();
 require('./startup/routes')(app);
--- startup/routes.js
+++ startup/routes.js
@@ -17,6 +17,14 @@ const testimonials = require('../routes/testimonial');
 const error = require('../middleware/error');
 
 module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization');
+    next();
+  });
+
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.static('public'));
