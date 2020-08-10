f3402e2ae5a4f983dc4e91f326ed3600d1009d0b
Sujit Libi <sujit_libi@hotmail.com>
2020-08-10 16:43:25 +0545

    rebuild project

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ app.use(function (req, res, next) {
   // next();
 
   res.header("Access-Control-Allow-Origin", "*") //* to give access to any origin
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token" //to give access to all the headers provided
-  );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
-    return res.status(200).json({});
-  }
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token" //to give access to all the headers provided
+  // );
+  // if (req.method === 'OPTIONS') {
+  //   res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
+  //   return res.status(200).json({});
+  // }
   next();
 });
 
