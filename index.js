418a36fcfefb4612f8b438f2f0beea25df3937fb
Sujit Libi <sujit_libi@hotmail.com>
2020-08-10 16:15:10 +0545

    minor change

--- index.js
+++ index.js
@@ -14,9 +14,20 @@ const app = express();
 // app.options('*', cors(corsOption));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
+  // next();
+
+  res.header("Access-Control-Allow-Origin", "*") //* to give access to any origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token" //to give access to all the headers provided
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
+    return res.status(200).json({});
+  }
   next();
 });
 
