f9760f3c7d022f95bb178b182e340d122e96c536
Sujit Libi <sujit_libi@hotmail.com>
2020-08-10 16:32:43 +0545

    rebuild project

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ const app = express();
 // app.use(cors(corsOption));
 // app.options('*', cors(corsOption));
 
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
+  // next();
+
+  res.header("Access-Control-Allow-Origin", "*") //* to give access to any origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token" //to give access to all the headers provided
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //to give access to all the methods provided
+    return res.status(200).json({});
+  }
+  next();
+});
 
 require('./startup/logging')();
 require('./startup/routes')(app);
--- startup/routes.js
+++ startup/routes.js
@@ -14,17 +14,12 @@ const customers = require('../routes/customers');
 const contactus = require('../routes/contactus');
 const testimonials = require('../routes/testimonial');
 
-const cors = require('../middleware/cors');
 const error = require('../middleware/error');
 
 module.exports = function (app) {
-
-  app.configure(function () {
-    app.use(cors);
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.static('public'));
-  });
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static('public'));
 
   app.use('/', home);
   app.use('/api/auth', auth);
