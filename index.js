633235164402b9f183fcf245b59a59b2c4299a29
Sujit Libi <sujit_libi@hotmail.com>
2020-08-11 09:31:29 +0545

    Cors issue resolve

--- index.js
+++ index.js
@@ -20,18 +20,17 @@ const app = express();
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  console.log("here")
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  console.log("set")
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-auth-token,Authorization,Content-Type,,application/json');
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,x-auth-token,Authorization,Content-Type,application/json");
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
   // Pass to next layer of middleware
   next();
