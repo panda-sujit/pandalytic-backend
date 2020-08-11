06b9a24fbd934b6d3991d6b397262bf34acfd384
Sujit Libi <sujit_libi@hotmail.com>
2020-08-11 09:20:32 +0545

    cors without using package

--- index.js
+++ index.js
@@ -13,7 +13,28 @@ const app = express();
 // app.use(cors(corsOption));
 // app.options('*', cors(corsOption));
 
-app.use(cors());
+// app.use(cors());
+// app.use(cors({origin: '*'}));
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-auth-token,Authorization,Content-Type,,application/json');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 require('./startup/logging')();
 require('./startup/routes')(app);
--- startup/routes.js
+++ startup/routes.js
@@ -18,13 +18,13 @@ const testimonials = require('../routes/testimonial');
 const error = require('../middleware/error');
 
 module.exports = function (app) {
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization');
-    next();
-  });
+  // app.use(function (req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", '*');
+  //   res.header("Access-Control-Allow-Credentials", true);
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  //   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization');
+  //   next();
+  // });
 
   app.use(cors());
 
