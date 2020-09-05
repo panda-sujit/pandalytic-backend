98b3581a8990d7d4b249f181c8f46e94183718e3
Sujit Libi <sujit_libi@hotmail.com>
2020-09-05 18:46:20 +0545

    Cors Issues resolved

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 
   // Request headers you wish to allow
-  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,x-auth-token,Authorization,Content-Type,application/json");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,Access-Control-Allow-Headers,content-type,x-auth-token,Authorization,Content-Type,application/json");
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
