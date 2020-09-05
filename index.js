ca95edfc34aa2502e5fe5a023be4cfc1d13dd08b
Sujit Libi <sujit_libi@hotmail.com>
2020-09-05 18:52:59 +0545

    Minor Change

--- index.js
+++ index.js
@@ -19,18 +19,22 @@ const app = express();
 // Add headers
 app.use(function (req, res, next) {
 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'DELETE, GET, POST, PUT, PATCH');
+
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  // res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 
   // Request headers you wish to allow
-  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,Access-Control-Allow-Headers,Origin,content-type,x-auth-token,Authorization,Content-Type,Accept,application/json");
+  // res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,Access-Control-Allow-Headers,Origin,content-type,x-auth-token,Authorization,Content-Type,Accept,application/json");
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  // res.setHeader("Access-Control-Allow-Credentials", true);
 
   // Pass to next layer of middleware
   next();
